6507851d14b2f4172dc643f6642b5a43ee42f213
Omar <oballesteros@ucol.mx>
2023-10-21 16:28:10 -0600

    Added all

--- App.js
+++ App.js
@@ -1,13 +1,49 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Component } from 'react';
+import { StyleSheet, Text, View, Button, Switch, TextInput, Alert } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default class App extends Component {
+  constructor (props) {
+    super (props)
+    this.state = {
+      switchValue: false,
+      name: "",
+    };
+  }
+
+  onChange = (value) => {
+    console.warn(`El switch cambiará a ${value}`)
+    this.setState({switchValue: value})
+  }
+
+  onPressLearnMore = () => {
+    Alert.alert(`${this.state.name}`, ` Es lo que se escribió`)
+  }
+  
+
+  render() {
+    return (
+      <View style={styles.container}>
+        
+        <TextInput
+        onChangeText={(text)=>this.setState({name:text})}
+        style={{marginBottom:5, borderBottomWidth:1}}
+        />
+        <Switch
+        onValueChange={()=> this.onChange(!this.state.switchValue)}
+        value={this.state.switchValue}/>
+
+        <Button
+        onPress={this.onPressLearnMore}
+        title='Learn More'
+        color="#841584"
+        accessibilityLabel='Learn more about this purple button'/>
+
+        <Text>Hello!</Text>
+        <StatusBar style="auto" />
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
